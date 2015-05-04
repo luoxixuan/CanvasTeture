01632b6f405c4db63875ab62de5d674d947c0ad0
luoxixuan <1139720869@qq.com>
2015-05-05 07:57:56 +0800

    modify coding style

--- MergeSortAlgorithm.h
+++ MergeSortAlgorithm.h
@@ -11,22 +11,23 @@ template <typename Comparable>
 void mergeSort(vector<Comparable> & a)
 {
 	vector<Comparable> tmpArray(a.size());
-		
-	mergeSort( a, tmpArray, 0, a.size() - 1);
+
+	mergeSort(a, tmpArray, 0, a.size() - 1);
 }
 
-/* * 
+/* *
  * 进行递归调用的内置方法.
  * a 是用来存放进行比较的对象的向量.
  * tmpArray 是用来存放子数组的向量.
  * left 是第一部分子数组最左边的位置.
  * right 是第一部分子数组最右边的位置.
-*/
+ */
 template <typename Comparable>
 void mergeSort(vector<Comparable> & a,
 			   vector<Comparable> & tmpArray, int left, int right)
 {
-	if ( left < right) {
+	if (left < right)
+	{
 		int center = (left + right) / 2;
 		mergeSort(a, tmpArray, left, center);
 		mergeSort(a, tmpArray, center + 1, right);
@@ -41,7 +42,7 @@ void mergeSort(vector<Comparable> & a,
  * leftPos 第二部分最左边的位置.
  * rightPos 是第二部分的起始位置.
  * rightend 是第二部分最右边的位置.
-*/
+ */
 template <typename Comparable>
 void merge(vector < Comparable> & a, vector<Comparable> & tmpArray,
 		   int leftPos, int rightPos, int rightEnd)
@@ -51,23 +52,29 @@ void merge(vector < Comparable> & a, vector<Comparable> & tmpArray,
 	int numElements = rightEnd - leftPos + 1;
 
 	// 主循环
-	while( leftPos <= leftEnd && rightPos <= rightEnd) {
-		if (a[ leftPos ] <= a[ rightPos]) {
+	while (leftPos <= leftEnd && rightPos <= rightEnd)
+	{
+		if (a[leftPos] <= a[rightPos])
+		{
 			tmpArray[tmpPos++] = a[leftPos++];
 		}
-		else {
+		else
+		{
 			tmpArray[tmpPos++] = a[rightPos++];
 		}
 	}
-	while ( leftPos <= leftEnd) {
+	while (leftPos <= leftEnd)
+	{
 		tmpArray[tmpPos++] = a[leftPos++];
 	}
-	while (rightPos <= rightEnd) {
+	while (rightPos <= rightEnd)
+	{
 		tmpArray[tmpPos++] = a[rightPos++];
 	}
 
 	// 把临时数组复制回a
-	for(int i = 0; i < numElements; i++,rightEnd-- ) {
+	for (int i = 0; i < numElements; i++, rightEnd--)
+	{
 		a[rightEnd] = tmpArray[rightEnd];
 	}
 }
